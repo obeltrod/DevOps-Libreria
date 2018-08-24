40348ccb4e282b3f26e5207987d6e55abd9decc1
obeltrod <42659367+obeltrod@users.noreply.github.com>
2018-08-24 11:01:47 -0500

    Creacion usuario Admin

--- DevOpsLibreria/CRUDLibreria/models.py
+++ DevOpsLibreria/CRUDLibreria/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Libro(models.Model):
+    nombre = models.CharField('nombre', max_length=100)
+    autor = models.CharField('autor', max_length=100)
+    isbn = models.CharField('isbn', max_length=100)
+    urlImagen = models.CharField('urlImagen', max_length=100)
+    editorial = models.CharField('editorial', max_length=100)
+    aPublicacion = models.CharField('aPublicacion', max_length=100)
+
+    class Meta:
+        verbose_name = 'libro'
+        verbose_name_plural = 'libros'
+        ordering = ['nombre']
+
+        def __unicode__(self):
+            return '%s %s %s' % (self.nombre, self.autor, self.isbn)
